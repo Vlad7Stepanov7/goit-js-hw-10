d171a480e65e24c9d9e6a290e78741943227ee29
Vlad7Stepanov7 <svlad4117@gmail.com>
2022-09-06 19:33:59 +0300

    сделал задание и разбил код по мелким файлам

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,10 @@
 
 export function fetchCountries(name) {
-    return fetch(`https://restcountries.com/v3.1/name/${name}`).then(response => response.json()).catch(error => console.log(error));
-        
-        
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+        .then(response => {
+            if (!response.ok) {
+                return;
+            }
+            return response.json();
+        })      
 }
--- src/index.js
+++ src/index.js
@@ -1,79 +1,48 @@
+
 import './css/styles.css';
+import { fetchCountries } from "./fetchCountries";
+import { listCountriesMarkup, infoCountryMarkup } from './markup.Countries';
+import { refs } from './refs';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-// import fetchCountries from "./fetchCountries";
+
 const debounce = require(`lodash.debounce`);
 
 const DEBOUNCE_DELAY = 300;
 
-const refs = {
-    countryInput: document.querySelector('#search-box'),
-    countryList: document.querySelector(`.country-list`),
-    countryInfo: document.querySelector(`.country-info`),
-};
-
-function fetchCountries(name) {
-    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
-        .then(response => response.json())
-        .catch(error => console.log(error));       
-}
 
 
-
-refs.countryInput.addEventListener(`input`, debounce(onFetchCountries, 300));
+refs.countryInput.addEventListener(`input`, debounce(onFetchCountries, DEBOUNCE_DELAY));
 
 function onFetchCountries(evt) {
     const nameCountry = evt.target.value.trim();
     
-    refs.countryList.innerHTML = ``;
-    refs.countryInfo.innerHTML = ``;
+   clearCountries()
 
     if (nameCountry === ``) {
        return
     }
     
+   
      fetchCountries(nameCountry)
          .then(countries => {
-             
-             if (countries.length > 10) {
-                 return console.log("Too many matches found. Please enter a more specific name.");    
+            
+             if (countries === undefined) {
+                 return Notify.failure("Oops, there is no country with that name");
+
+             } else if (countries.length > 10) {
+                 return Notify.info("Too many matches found. Please enter a more specific name.");    
 
              } else if (countries.length === 1) {
+                
                  return infoCountryMarkup(countries);
              }
 
-             console.log(countries);
-
             return listCountriesMarkup(countries);
-         });
+         }).catch(error => console.log(error)); 
+               
 }
 
-function listCountriesMarkup(countries) {
-    
-    const markupCountries = countries.map(country => {
-        const { name, flags} = country;
-
-     return`<li>
-     <img src="${flags.svg}" width="20" height="20" />
-     <p>${name.common}</p>
-     </li>`
-    }).join(``);
-
-    return refs.countryList.insertAdjacentHTML(`beforeend`, markupCountries);
-}
-
-function infoCountryMarkup(country) {
-    
-    const markupCountry = country.map(country => {
-        const { name, flags, capital, population, languages } = country;
-
-     return`<li>
-     <img src="${flags.svg}" width="20" height="20" />
-     <p>${name.official}</p>
-     <p>Capital: ${capital}</p>
-     <p>Population: ${population}</p>
-     <p>languages: ${languages}</p>
-     </li>`
-    }).join(``);
-
-    return refs.countryInfo.insertAdjacentHTML(`beforeend`, markupCountry);
+function clearCountries() {
+ refs.countryList.innerHTML = ``;
+    refs.countryInfo.innerHTML = ``;
 }
